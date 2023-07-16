d1354411be989320502d60d090a368ffd8118782
Leticia Rijo <leticia.rijo@safeweb.com.br>
2023-07-15 21:55:09 -0300

    ADD Login failed no Form e Update no commands

--- cypress/e2e/FormAuthentication.cy.js
+++ cypress/e2e/FormAuthentication.cy.js
@@ -7,16 +7,57 @@ describe('Form Authentication', () => {
             cy.get('li a[href="/login"]').click()
 
             cy.fixture('dados').then((dados) => {
-                cy.get("#username").type(dados.login.user)
-                cy.get("#password").type(dados.login.password, {log: false})
+                cy.get('#username').type(dados.login.user)
+                cy.get('#password').type(dados.login.password, {log: false})
             })
 
             cy.get('button.radius').contains("Login").click()
 
             cy.url().should('eq', 'https://the-internet.herokuapp.com/secure')
 
-            cy.contains(" You logged into a secure area! ")
+            cy.contains(' You logged into a secure area! ')
 
         })
+
+        it('Login failed - Incorrect password', () => {
+            
+            cy.AccessTheInternet()
+
+            cy.get('li a[href="/login"]').click()
+
+            cy.fixture('dados').then((dados) => {
+                cy.get('#username').type(dados.login.user)
+            })
+
+            cy.get('#password').type('passwordfake')
+
+            cy.get('button.radius').contains("Login").click()
+
+            cy.url().should('eq', 'https://the-internet.herokuapp.com/login')
+
+            cy.contains(' Your password is invalid! ')
+
+        })
+
+        it('Login failed - Incorrect password', () => {
+            
+            cy.AccessTheInternet()
+
+            cy.get('li a[href="/login"]').click()
+
+            cy.fixture('dados').then((dados) => {
+                cy.get('#username').type(dados.login.user)
+            })
+
+            cy.get('#password').type('passwordfake')
+
+            cy.get('button.radius').contains("Login").click()
+
+            cy.url().should('eq', 'https://the-internet.herokuapp.com/login')
+
+            cy.contains(' Your password is invalid! ')
+
+        })
+
 
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,5 @@
 Cypress.Commands.add('AccessTheInternet', () => {
     cy.visit('https://the-internet.herokuapp.com/')
-
-    Cypress.on('uncaught:exception', (err, runnable) => {
-        return false;       
-    })
-});
+})
 
     
