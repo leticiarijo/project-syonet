a08b5c93592744cc0df45921898471d8c6a90ac0
leticiarijo <49538833+leticiarijo@users.noreply.github.com>
2023-07-15 21:56:28 -0300

    Revert "ADD Login failed no Form e Update no commands"

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,5 +1,9 @@
 Cypress.Commands.add('AccessTheInternet', () => {
     cy.visit('https://the-internet.herokuapp.com/')
-})
+
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        return false;       
+    })
+});
 
     
