6d89683d96d1b06bd448226e9125c45748103485
Leticia Rijo <leticia.rijo@safeweb.com.br>
2023-07-15 21:59:25 -0300

    Update Form Authentication

--- cypress/e2e/FormAuthentication.cy.js
+++ cypress/e2e/FormAuthentication.cy.js
@@ -39,23 +39,23 @@ describe('Form Authentication', () => {
 
         })
 
-        it('Login failed - Incorrect password', () => {
+        it('Login failed - Incorrect user', () => {
             
             cy.AccessTheInternet()
 
             cy.get('li a[href="/login"]').click()
 
-            cy.fixture('dados').then((dados) => {
-                cy.get('#username').type(dados.login.user)
-            })
+            cy.get('#username').type('userFaker')
 
-            cy.get('#password').type('passwordfake')
+            cy.fixture('dados').then((dados) => {
+                cy.get('#password').type(dados.login.password, {log: false})
+            })
 
             cy.get('button.radius').contains("Login").click()
 
             cy.url().should('eq', 'https://the-internet.herokuapp.com/login')
 
-            cy.contains(' Your password is invalid! ')
+            cy.contains(' Your username is invalid! ')
 
         })
 
