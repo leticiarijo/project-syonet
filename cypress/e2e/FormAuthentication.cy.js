0e9f76eec8202e09f8ac7fd0739cba7e7ea7693b
Leticia Rijo <leticia.rijo@safeweb.com.br>
2023-07-16 20:26:52 -0300

    Update FormAuthentication Adicionado mais um cenário de teste

--- cypress/e2e/FormAuthentication.cy.js
+++ cypress/e2e/FormAuthentication.cy.js
@@ -1,11 +1,14 @@
 describe('Form Authentication', () => {
         
+
         it('Login success', () => {
 
             cy.AccessTheInternet()
 
             cy.get('li a[href="/login"]').click()
 
+        //Aqui foi utilizado uma fixture, a qual não está no repositório, assim conseguimos protejer dados sensiveis como senha e, em determinados momentos, o usuário
+        //a fixture está sendo ignorada pelo git ignore
             cy.fixture('dados').then((dados) => {
                 cy.get('#username').type(dados.login.user)
                 cy.get('#password').type(dados.login.password, {log: false})
@@ -51,6 +54,24 @@ describe('Form Authentication', () => {
                 cy.get('#password').type(dados.login.password, {log: false})
             })
 
+            cy.get('button.radius').contains("Login").click()
+
+            cy.url().should('eq', 'https://the-internet.herokuapp.com/login')
+
+            cy.contains(' Your username is invalid! ')
+
+        })
+
+        it('Login failed - Incorrect user and password', () => {
+            
+            cy.AccessTheInternet()
+
+            cy.get('li a[href="/login"]').click()
+
+            cy.get('#username').type('userFaker')
+
+            cy.get('#password').type('passwordfake')
+           
             cy.get('button.radius').contains("Login").click()
 
             cy.url().should('eq', 'https://the-internet.herokuapp.com/login')
